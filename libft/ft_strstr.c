d5d1025ac9da41bb7eade85a9e1807fdd024f16b
Allah <freddyzet@gmail.com>
2019-01-22 15:59:30 +0200

    Eben boben it's working

--- libft/ft_strstr.c
+++ libft/ft_strstr.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../../Desktop/iruban2/libft.h"
+#include "includes/libft.h"
 
 char	*ft_strstr(const char *haystack, const char *needle)
 {
